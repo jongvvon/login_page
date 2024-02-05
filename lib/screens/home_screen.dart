2a71302434895c22dbf90edfb81cf52d552d2fe4
jongvvon <jwjang7024@gmail.com>
2024-02-05 21:31:50 +0900

    home screen fixed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:welcome/screens/onboding_screen.dart';
+import 'package:welcome/screens/sign_in_screen.dart';
 
 class homeScreen extends StatefulWidget {
   const homeScreen({super.key});
@@ -11,6 +13,27 @@ class homeScreen extends StatefulWidget {
 class _homeScreenState extends State<homeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SignInScreen()),
+                  );
+                },
+                icon: const Icon(Icons.backspace),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
