79ad39419922ef5c41bcf6830c91e5ded6e65c9d
jongvvon <jwjang7024@gmail.com>
2024-02-10 17:32:34 +0900

    240210

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,171 @@
 import 'package:flutter/material.dart';
+import 'package:welcome/screens/onboding_screen.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  List symptoms = [
+    "Temperature",
+    "Snuffle",
+    "Fever",
+    "Cough",
+    "Cold",
+  ];
+
+  List imgs = [];
+
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return SingleChildScrollView(
+      padding: const EdgeInsets.only(
+        top: 40,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Hello Alex",
+                  style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Color(0xFF556b2f),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const OnboardingScreen()),
+                  );
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF556b2f),
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 6,
+                        spreadRadius: 4,
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                        ),
+                        child: const Icon(
+                          Icons.add,
+                          color: Color(0xFF556b2f),
+                          size: 35,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      const Text(
+                        "Clinic Visit",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      const Text(
+                        "Make an appointment",
+                        style: TextStyle(
+                          color: Colors.white54,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF556b2f),
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 6,
+                        spreadRadius: 4,
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                        ),
+                        child: const Icon(
+                          Icons.add,
+                          color: Color(0xFF556b2f),
+                          size: 35,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      const Text(
+                        "Clinic Visit",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      const Text(
+                        "Make an appointment",
+                        style: TextStyle(
+                          color: Colors.white54,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/widgets/navbar_roots.dart
+++ lib/widgets/navbar_roots.dart
@@ -11,7 +11,7 @@ class NavBarRoots extends StatefulWidget {
 class _NavBarRootsState extends State<NavBarRoots> {
   int _selectedIndex = 0;
   final _screens = [
-    const HomeScreen(),
+    HomeScreen(),
     Container(),
     Container(),
     Container(),
@@ -22,6 +22,7 @@ class _NavBarRootsState extends State<NavBarRoots> {
     // 현재 개발하고 있는 iOS 환경의 경우 홈 인디케이터의 영향으로 safeArea 가 설정되어 있음
     // 하단의 코드는 안드로이드 내부에서는 정상 동작하는 코드였지만 iOS 환경에서는 bottom 11px overflow 발생
     // 해당 문제를 해결하기 위해 SafeArea 동작을 제한하였음. 크로스 플랫폼 개발시에 신경써야할 문제인듯 싶다.
+    // 크로스 플랫폼 개발시에 문제가 되는 사항이 아닐까 하는 의문이 듭니다....
     return SafeArea(
       top: false,
       child: Scaffold(
